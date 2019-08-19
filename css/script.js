4446614936201eac05bd5eaa7372dc717dc4d4e9
Mariiba <emmanuelmariiba@gmail.com>
2019-08-19 12:57:05 +0300

    add the new script to script.js

--- css/script.js
+++ css/script.js
@@ -49,54 +49,81 @@ function validation(){
 
      //male Akan names
     var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    //Akan female names
+    var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+     
+    //data collection
+    var date = document.getElementById("dob");
+    var dd = date;
+    var month = document.getElementById("month");
+    var mm = month;
+    var year = document.getElementById("year");
+    var yy = parseInt(year.toString().slice(2,4));
+    var cc = Math.ceil(year/100);
+
+
+    //formula
+    var calculator= Math.floor(((cc/4)-2*cc-1)+(5*yy/4)+((26*(mm+1)/10)+dd)%7);
+     
+
+    //male names
+
+    if(male == true && calculator == 1){
+        document.getElementById("result").innerHTML=("Your Akan name is " + maleNames[0] + ".")
+    }
+    else if(male == true && calculator == 2){
+        document.getElementById("result").innerHTML=("Your Akan name is " + maleNames[1] + ".")
+    }
+    else if(male == true && calculator == 3){
+        document.getElementById("result").innerHTML=("Your Akan name is " + maleNames[2] + ".")
+    }
+    else if(male == true && calculator == 4){
+        document.getElementById("result").innerHTML=("Your Akan name is " + maleNames[3] + ".")
+    }
+    else if(male == true && calculator == 5){
+        document.getElementById("result").innerHTML=("Your Akan name is " + maleNames[4] + ".")
+    }
+    else if(male == true && calculator == 6){
+        document.getElementById("result").innerHTML=("Your Akan name is " + maleNames[5] + ".")
+    }
+    else if(male == true && calculator == 7){
+        document.getElementById("result").innerHTML=("Your Akan name is " + maleNames[6] + ".")
+    }
+    else{
+        alert("The information you entered is invalid!")
+    }
+
+    //female names
+
+    if(female == true && calculator == 1){
+        document.getElementById("result").innerHTML=("Your Akan name is " + femaleNames[0] + ".")
+    }
+    else if(female == true && calculator == 2){
+        document.getElementById("result").innerHTML=("Your Akan name is " + femaleNames[1] +  ".")
+    }
+    else if(female == true && calculator == 3){
+        document.getElementById("result").innerHTML=("Your Akan name is " + femaleNames[2] +  ".")
+    }
+    else if(female == true && calculator == 4){
+        document.getElementById("result").innerHTML=("Your Akan name is " + femaleNames[3] +  ".")
+    }
+    else if(female == true && calculator == 5){
+        document.getElementById("result").innerHTML=("Your Akan name is " + femaleNames[4] +  ".")
+    }
+    else if(female == true && calculator == 6){
+        document.getElementById("result").innerHTML=("Your Akan name is " + femaleNames[5] +  ".")
+    }
+    else if(female == true && calculator == 7){
+        document.getElementById("result").innerHTML=("Your Akan name is " + femaleNames[6] +  ".")
+    }
+    else{
+        alert("The information you entered is invalid!")
+    }
+
+
+
+
+
     
-
-
-
-    // if(date=="" || month=="" || year==""){
-    //     document.getElementById("result").innerHTML="Please enter the required information"
-    //     return false;
-    // }
-    // else{
-    //    true;
-    // };
-//     var cC=document.getElementById("year").slice(0,1);
-//     var yY=document.getElementById("year").slice(2,3);
-//     var mM=document.getElementById("month");
-//     var dD=document.getElementById("dob");
-//    var calculator= math.floor(( ( (cC/4) -2*cC-1) + ((5*yY/4) ) + ((26*(mM+1)/10)) + dD ) % 7);
-//    var maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-//    var femaleNames=["Akoswa", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-//     //male function
-//     if(male==true && calculator==1){
-//        document.getElementById("result").innerHTML=maleNames[0];
-//     }else if(male==true && calculator==2){
-//         document.getElementById("result").innerHTML=maleNames[1];
-//     }else if(male==true && calculator==3){
-//         document.getElementById("result").innerHTML=maleNames[2];
-//     }else if(male==true && calculator==4){
-//         document.getElementById("result").innerHTML=maleNames[3];
-//     }else if(male==true && calculator==5){
-//         document.getElementById("result").innerHTML=maleNames[4];
-//     }else if(male==true && calculator==6){
-//         document.getElementById("result").innerHTML=maleNames[5];
-//     }else{
-//         document.getElementById("result")=alert("enter correct info")
-//     }
-//     //female function
-//     if(female==true && calculator==1){
-//         document.getElementById("result").innerHTML=femaleNames[o];
-//     }else if(female==2 && calculator==2){
-//         document.getElementById("result").innerHTML=femaleNames[1];
-//     }else if(female==2 && calculator==3){
-//         document.getElementById("result").innerHTML=femaleNames[2];
-//     }else if(female==2 && calculator==4){
-//         document.getElementById("result").innerHTML=femaleNames[3];
-//     }else if(female==2 && calculator==5){
-//         document.getElementById("result").innerHTML=femaleNames[4];
-//     }else if(female==2 && calculator==6){
-//         document.getElementById("result").innerHTML=femaleNames[5];
-//     }
-   
- 
-   
