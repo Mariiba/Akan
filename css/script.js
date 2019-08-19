c74ae8f349638c1cb07f13aaf71221496b1e382b
Mariiba <emmanuelmariiba@gmail.com>
2019-08-19 08:32:57 +0300

    add js male and female funtions

--- css/script.js
+++ css/script.js
@@ -14,16 +14,40 @@ function validation(){
     var mM=document.getElementById("month");
     var dD=document.getElementById("dob");
    var calculator= function(dD,mM,yY,cC){
-       return ( ( (cC/4) -2*cC-1) + ((5*yY/4) ) + ((26*(mM+1)/10)) + dD ) % 7
+       return Math.floor(( ( (cC/4) -2*cC-1) + ((5*yY/4) ) + ((26*(mM+1)/10)) + dD ) % 7)
    };
    var maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
    var femaleNames=["Akoswa", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    //male function
+    if(male==true && calculator==1){
+       document.getElementById("result").innerHTML=maleNames[0];
+    }else if(male==true && calculator==2){
+        document.getElementById("result").innerHTML=maleNames[1];
+    }else if(male==true && calculator==3){
+        document.getElementById("result").innerHTML=maleNames[2];
+    }else if(male==true && calculator==4){
+        document.getElementById("result").innerHTML=maleNames[3];
+    }else if(male==true && calculator==5){
+        document.getElementById("result").innerHTML=maleNames[4];
+    }else if(male==true && calculator==6){
+        document.getElementById("result").innerHTML=maleNames[5];
+    }else{
+        document.getElementById("result")=alert("enter correct info")
+    }
+    //female function
+    if(female==true && calculator==1){
+        document.getElementById("result").innerHTML=femaleNames[o];
+    }else if(female==2 && calculator==2){
+        document.getElementById("result").innerHTML=femaleNames[1];
+    }else if(female==2 && calculator==3){
+        document.getElementById("result").innerHTML=femaleNames[2];
+    }else if(female==2 && calculator==4){
+        document.getElementById("result").innerHTML=femaleNames[3];
+    }else if(female==2 && calculator==5){
+        document.getElementById("result").innerHTML=femaleNames[4];
+    }else if(female==2 && calculator==6){
+        document.getElementById("result").innerHTML=femaleNames[5];
+    }
    
-   var gender=document.getElementsByName("gender");
-   var i=0;
-   while(i<gender.length){
-       if (gender[i].checked)
-       var myGender=gender[i].value;
-   };
-   alert(myGender)
+ 
    
